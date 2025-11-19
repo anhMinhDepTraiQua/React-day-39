a37e5d88d4c16a1cc5bd5750fda9b3462f609af2
anhMinhDepTraiQua <ducanhbanche@gmail.com>
2025-11-19 13:22:19 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoApp from "./pages/TodoApp";
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter basename="React-day-39" >
       <Routes>
-<Route path="/" element={<TodoApp />} />
-      </Routes>
+        <Route path="/" element={<TodoApp />} />
+        </Routes>
     </BrowserRouter>
   );
 }
